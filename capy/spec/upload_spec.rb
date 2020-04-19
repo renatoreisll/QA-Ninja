680df0402f0433e139d667de711f4f75b4e2e4c0
Renato Reis <renatoreis@live.com>
2020-04-19 12:39:32 -0400

    Inclusão do arquivo upload_spec.rb e adição da pasta fixtures contendo arquivos. Alteração de tempo máximo no spechelper para 5 seg.

---
+++ capy/spec/upload_spec.rb
@@ -0,0 +1,35 @@
+
+describe 'Upload', :upload do
+
+    before(:each) do
+        @arquivo = Dir.pwd + '/spec/fixtures/arquivo.txt'
+        @imagem = Dir.pwd + '/spec/fixtures/imagem.jpg'
+        visit 'https://training-wheels-protocol.herokuapp.com/upload'
+    end
+
+    it 'Upload com o arquivo texto' do
+    
+        attach_file('file-upload', @arquivo)
+        click_button 'Upload'
+
+        div_arquivo = find('#uploaded-file')
+        expect(div_arquivo.text).to eql 'arquivo.txt'
+
+    end
+
+    it 'Upload de imagem' do
+
+        attach_file('file-upload', @imagem)
+        click_button 'Upload'
+
+        img = find('#new-image')
+        expect(img[:src]).to include 'imagem.jpg'
+
+    end
+
+
+    after(:each) do
+    sleep(3)
+    end
+
+end
