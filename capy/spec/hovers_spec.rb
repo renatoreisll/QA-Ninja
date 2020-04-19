8b2af39a0ad5ecf7c1fbded7af603b7c04ad6a19
Renato Reis <renatoreis@live.com>
2020-04-19 15:58:28 -0400

    Alteração do arquivo hovers_spec.rb com a inclusão do Regex.

--- capy/spec/hovers_spec.rb
+++ capy/spec/hovers_spec.rb
@@ -6,16 +6,23 @@ describe 'Mouse Hover', :hovers do
 
     ## O Blade é pesquisado pelo elemento img alt...
     it 'Quando passo mouse sobre o Blade' do
-        card = find('img[alt=Blade]')
+        card = find('img[alt*=Blade]')
         card.hover
 
         expect(page).to have_content 'Nome: Blade'
 
     end
 
-    #Já o Pantera Negra e o Spiderman foi necessário colocar entre aspas duplas para localizar em forma de string.
+    ## Regex: permite buscar por seletores usando os comandos:
+    ## * = contém
+    ## ^ = começa com
+    ## $ = termina com
+    ## Obs.: não se usa as aspas.
+
+
+    ## O Pantera Negra e o Spiderman foi necessário colocar entre aspas duplas para localizar em forma de string.
     it 'Quando passo mouse sobre o Pantera Negra' do
-        card = find('img[alt="Pantera Negra"]')
+        card = find('img[alt^=Pantera]')
         card.hover
 
         expect(page).to have_content 'Nome: Pantera Negra'
@@ -23,7 +30,7 @@ describe 'Mouse Hover', :hovers do
     end
 
     it 'Quando passo mouse sobre o Homem Aranha' do
-        card = find('img[alt="Homem Aranha"]')
+        card = find('img[alt$=Aranha]')
         card.hover
 
         expect(page).to have_content 'Nome: Homem Aranha'
