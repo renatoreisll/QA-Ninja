45731d2756046a0aa6ee72ac1fe8c04bf4730d75
Renato Reis <renatoreis@live.com>
2020-04-19 21:52:09 -0400

    Inclusão do arquivo ids_dinamicos_spec.rb.

---
+++ capy/spec/id_dinamico_spec.rb
@@ -0,0 +1,22 @@
+describe 'IDs dinâmicos', :teste do
+
+    before(:each) do
+        visit 'https://training-wheels-protocol.herokuapp.com/access'
+    end
+
+    ## $ = termina com
+    ## ^ = começa com
+    ## * = contém
+
+    it 'Cadastro' do
+        find('input[id$=UsernameInput').set 'renato'
+        find('input[id^=PasswordInput').set '123123'
+        find('a[id*=GetStartedButton').click
+
+        expect(page).to have_content 'Dados enviados. Aguarde aprovação do seu cadastro!'
+
+        sleep(3)
+
+    end
+    
+end
